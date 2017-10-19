4c43defcfbcdeec9f886de015ac41c72acd23bc7
Andrew Zhu <mommothazaz123@gmail.com>
2017-10-19 14:49:23 -0700

    moved file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AndrewOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AndrewOp.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.test;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
